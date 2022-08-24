73bb6e75c0c6f5d8004d8c5965edae35d3c3fdde
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-24 21:00:07 +0530

    Custom Exception handling

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -7,11 +7,16 @@ namespace MoodAnalyzerProblem
         static void Main(string[] args)
         {
             Console.WriteLine("-------------Mood Analyser------------");
-            Console.WriteLine("Enter a message to test mood : ");
-            string message =Console.ReadLine();
-            MoodAnalyse moodAnalyse = new MoodAnalyse();
-            string result=moodAnalyse.AnalyseMood(message);
-            Console.WriteLine(result);
+            try
+            {
+                MoodAnalyse moodAnalyse = new MoodAnalyse();
+                string result = moodAnalyse.AnalyseMood();
+                Console.WriteLine(result);
+            }
+            catch (CustomMoodAnalyserException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
